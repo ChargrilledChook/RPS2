1f4bd44102996dd8c4dadc094f9f90f4b1309f70
chook <dylanw212@gmail.com>
2020-11-18 12:28:26 +1100

    add various configs for prettier and eslint

--- scripts.js
+++ scripts.js
@@ -1,91 +1,89 @@
-const win = 'You win!';
+const win = "You win!";
 
-const loss = 'You lose!';
+const loss = "You lose!";
 
-const draw = 'Draw!';
+const draw = "Draw!";
 
 const outcomes = {
-  rock : { rock : draw, paper : loss, scissors : win },
-  paper : { rock : win, paper : draw, scissors : loss },
-  scissors : { rock : loss, paper : win, scissors : draw }
+  rock: { rock: draw, paper: loss, scissors: win },
+  paper: { rock: win, paper: draw, scissors: loss },
+  scissors: { rock: loss, paper: win, scissors: draw },
 };
 
 function round() {
   const player = playerChoice();
   const computer = computerChoice();
   return outcomes[player][computer];
-};
+}
 
 function match() {
   let playerScore = 0;
   let computerScore = 0;
 
-  while(playerScore < 6 && computerScore < 6) {
-
-  }
+  while (playerScore < 6 && computerScore < 6) {}
 }
 
-const HumanPlayer = function(name) {
+const HumanPlayer = function (name) {
   this.name = name;
   this.score = 0;
-  this.give_point = () => this.score +=1;
-  this.default_move = () => 'scissors';
+  this.give_point = () => (this.score += 1);
+  this.default_move = () => "scissors";
   this.pick_move = () => prompt();
-}
+};
 
-const ComputerPlayer = function(name) {
+const ComputerPlayer = function (name) {
   this.name = name;
   this.score = 0;
   this.give_point = () => score++;
   this.pick_move = () => {
-    const choices = ['rock', 'paper', 'scissors']
-    return choices[Math.floor(Math.random() * choices.length)]
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
   };
-}
+};
 
 const ComputerFac = (name) => {
   let score = 0;
   const getScore = () => score;
   const givePoint = () => {
     score++;
-    return 'Computer scores!'
-  }
+    return "Computer scores!";
+  };
   pickMove = () => {
-    const choices = ['rock', 'paper', 'scissors']
-    return choices[Math.floor(Math.random() * choices.length)]
-  }
-  return { name, getScore, givePoint, pickMove }
-}
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+  };
+  return { name, getScore, givePoint, pickMove };
+};
 
 const PlayerFac = (name) => {
   let score = 0;
   const getScore = () => score;
   const givePoint = () => {
     score++;
-    return 'Computer scores!'
-  }
+    return "Computer scores!";
+  };
   pickMove = () => {
-    const choices = ['rock', 'paper', 'scissors']
-    return choices[Math.floor(Math.random() * choices.length)]
-  }
-  return { name, getScore, givePoint, pickMove }
-}
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+  };
+  return { name, getScore, givePoint, pickMove };
+};
 
-const human = new HumanPlayer('Bill')
-const ai = new ComputerPlayer('TIS-5000')
+const human = new HumanPlayer("Bill");
+const ai = new ComputerPlayer("TIS-5000");
 
-function match(player1, player2) {
+/* function match(player1, player2) {
   while(player1.getscore < 6 && player2.getScore < 6) {
 
   }
-}
+} */
 
-switch(result) {
+switch (result) {
   case win:
     player1.givePoint();
     break;
   case loss:
-    player2.givePoint()
+    player2.givePoint();
   default:
     break;
 }
--- simpleObjVer.js
+++ simpleObjVer.js
@@ -1,15 +1,15 @@
 function match() {
-  const player = { score : 0, name : 'Human' }
-  const computer = { score : 0, name : 'AI' }
+  const player = { score: 0, name: "Human" };
+  const computer = { score: 0, name: "AI" };
 
   const outcomes = {
-  rock : { rock : 'draw', paper : 'loss', scissors : 'win' },
-  paper : { rock : 'win', paper : 'draw', scissors : 'loss' },
-  scissors : { rock : 'loss', paper : 'win', scissors : 'draw' }
+    rock: { rock: "draw", paper: "loss", scissors: "win" },
+    paper: { rock: "win", paper: "draw", scissors: "loss" },
+    scissors: { rock: "loss", paper: "win", scissors: "draw" },
   };
 
   while (player.score < 5 && computer.score < 5) {
-    playRound(player, computer, outcomes)
+    playRound(player, computer, outcomes);
   }
 
   let winner;
@@ -17,35 +17,38 @@ function match() {
 
   if (player.score > computer.score) {
     winner = player;
-    loser = computer
+    loser = computer;
   } else {
-     winner = computer;
-     loser = player;
+    winner = computer;
+    loser = player;
   }
 
-  return `${winner.name} wins ${winner.score} to ${loser.score}!`
+  return `${winner.name} wins ${winner.score} to ${loser.score}!`;
 }
 
 function playRound(player1, player2, outcomes) {
-  result = outcomes[chooser()][chooser()]
-  givePoint(player1, player2, result)
+  let result = outcomes[chooser()][chooser()];
+  givePoint(player1, player2, result);
 }
 
-function givePoint (player1, player2, result) {
-  switch(result) {
-    case 'win':
+function givePoint(player1, player2, result) {
+  switch (result) {
+    case "win":
       player1.score += 1;
       break;
-    case 'loss':
+    case "loss":
       player2.score += 1;
-    case 'draw':
       break;
+    case "draw":
+      break;
+    default:
+        return 'Error!'
   }
 }
 
 function chooser() {
-  const choices = ['rock', 'paper', 'scissors']
-  return choices[Math.floor(Math.random() * choices.length)]
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-match()
+match();
